a4febbb4224baa2df3e6b468d1aab7de3cb1622d
ciedar <dciesla545@gmail.com>
2023-02-16 19:46:21 +0100

    login function(js)

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
 "use strict";
 
-console.log("chuj dupa cipa");
+const account1 = {
+    owner: "Dariusz Cieśla",
+    login: "darcie",
+    pin: 1111
+}
+
+const account2 = {
+    owner: "Mateusz Woś",
+    login: "matwos",
+    pin: 2222
+}
+
+const listOfAccounts = [account1, account2];
+
+
+
+const loginButton = document.querySelector(".login--enter");
+const loginInput = document.querySelector(".login--input");
+const passwordInput = document.querySelector(".password--input");
+
+let currentAccount;
+
+const loginFunction = function () {
+    listOfAccounts.find(function (current) {
+        if (loginInput.value === current.login & Number(passwordInput.value) === current.pin) {
+            currentAccount = current;
+            console.log(currentAccount)
+        }
+    })
+}
+
+
+loginButton.addEventListener("click", function (a) {
+    a.preventDefault();
+    loginFunction();
+});
