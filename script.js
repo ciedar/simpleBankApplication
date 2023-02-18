7d6ff3b668a7ce19e52b9e5cc048950f900216df
ciedar <dciesla545@gmail.com>
2023-02-18 11:45:09 +0100

    calc current balance of currentAccount

--- script.js
+++ script.js
@@ -3,27 +3,36 @@
 const account1 = {
     owner: "Dariusz Cieśla",
     login: "darcie",
-    pin: 1111
+    pin: 1111,
+    history: [122, 1698, -129, -872, 76, -50, 5298]
 }
 
 const account2 = {
     owner: "Mateusz Woś",
     login: "matwos",
-    pin: 2222
+    pin: 2222,
+    history: [122, 168, -129, -82, 76, -508, 298]
 }
 
 const listOfAccounts = [account1, account2];
 
-
-
+let currentAccount;
 const loginButton = document.querySelector(".login--enter");
 const loginInput = document.querySelector(".login--input");
 const passwordInput = document.querySelector(".password--input");
 const mainVisable = document.querySelector(".main");
 const navbar = document.querySelector(".nav--bar");
 
-let currentAccount;
-document.na
+const currentBalance = function (account) {
+    account.balance = account.history.reduce(function (value1, value2) {
+        return value1 + value2;
+    })
+    document.querySelector(".current--balance").textContent = `${account.balance} PLN`;
+}
+
+
+
+
 const loginFunction = function () {
     listOfAccounts.find(function (current) {
         if (loginInput.value === current.login & Number(passwordInput.value) === current.pin) {
@@ -36,7 +45,21 @@ const loginFunction = function () {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 loginButton.addEventListener("click", function (a) {
     a.preventDefault();
     loginFunction();
+    currentBalance(currentAccount);
 });
+
