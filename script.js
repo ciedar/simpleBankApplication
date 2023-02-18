7ed3d6804bc125f4c7de0846e1f6022d519dc39f
ciedar <dciesla545@gmail.com>
2023-02-18 10:36:06 +0100

    navBar opacity

--- script.js
+++ script.js
@@ -20,15 +20,17 @@ const loginButton = document.querySelector(".login--enter");
 const loginInput = document.querySelector(".login--input");
 const passwordInput = document.querySelector(".password--input");
 const mainVisable = document.querySelector(".main");
+const navbar = document.querySelector(".nav--bar");
 
 let currentAccount;
-
+document.na
 const loginFunction = function () {
     listOfAccounts.find(function (current) {
         if (loginInput.value === current.login & Number(passwordInput.value) === current.pin) {
             currentAccount = current;
             console.log(currentAccount)
             mainVisable.style.opacity = 1;
+            navbar.style.opacity = 0;
         }
     })
 }
